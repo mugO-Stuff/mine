a684ef33c762c99a8d3eacb1d445571591d47481
S7venSantos <santos.worker20@gmail.com>
2026-04-08 23:49:26 -0300

    Login

---
+++ create_admin.py
@@ -0,0 +1,18 @@
+from app import app, db, User
+
+with app.app_context():
+    # Verifica se já existe um usuário admin
+    admin = User.query.filter_by(nome='Gestão').first()
+    if not admin:
+        admin = User(
+            nome='Gestão',
+            cargo='Gerencia',
+            senha='13092026',  # senha em texto puro, igual ao sistema atual
+            status='approved',
+            grau=3
+        )
+        db.session.add(admin)
+        db.session.commit()
+        print('Usuário admin criado com sucesso!')
+    else:
+        print('Usuário admin já existe.')
